4f7191043ccf6d2d4a3c7d5174b42f1489cf405a
Victor Gramolini <victorluiz_soares@yahoo.com.br>
2023-03-02 16:58:19 -0300

    [feature/FMFAB-2] - setting up the project

---
+++ src/main/java/br/com/financeManagerForAll/financeManagerForAllBackend/financeManagerForAllBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.financeManagerForAll.financeManagerForAllBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class financeManagerForAllBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(financeManagerForAllBackendApplication.class, args);
+	}
+
+}
